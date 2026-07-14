42d7822d3741bb2de264debd2e91ea477cb98059
AZAMBOU Marcel <azambou@mac.home>
2026-07-13 20:45:50 -0400

    Initial commit

---
+++ FamilyFinanceApp/FamilyFinanceApp/FamilyFinanceAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FamilyFinanceAppApp.swift
+//  FamilyFinanceApp
+//
+//  Created by AZAMBOU Marcel on 2026-07-13.
+//
+
+import SwiftUI
+
+@main
+struct FamilyFinanceAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
